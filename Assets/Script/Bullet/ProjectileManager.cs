609d33d4ba0b3194c4342a1ac1f2d1e3ee9338f0
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2025-07-04 12:34:07 +0900

    a

--- Assets/Script/Bullet/ProjectileManager.cs
+++ Assets/Script/Bullet/ProjectileManager.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+/// <summary>
+/// 弾をプールで管理するクラス
+/// </summary>
+public class ProjectileManager : MonoBehaviour {
+    // 自身の取得
+    public static ProjectileManager Instance { get; private set; } = null;
+    
+    // 弾のPrefab
+    [SerializeField]
+    private GameObject projectilePrefab;
+    // プールするサイズ
+    [SerializeField]
+    private int initialPoolSize = 20;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
 }
